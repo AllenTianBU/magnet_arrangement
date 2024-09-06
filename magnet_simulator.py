b78f3a2cf1de5eaf288ffb0e34cdc9c398187a34
AllenTianBU <ztian@bu.edu>
2024-09-06 13:44:03 +1000

    Add files via upload

--- magnet_simulator.py
+++ magnet_simulator.py
@@ -1,86 +1,117 @@
-from multiprocessing.util import MAXFD
-
 import numpy as np
 import magpylib as magpy
 import matplotlib.pyplot as plt
-from plotly.graph_objs.layout.xaxis import Minor
 
-#  Configuration
-n_magnets = 16 #number of magnet per ring
-n_rings = 8 #number of rings
-cube_length = 10 #length of cube in mm
-magnet_strength = 5492 #magnet strength in gauss
+# Configuration
+n_magnets = 18 # Number of magnet per ring
+n_rings = 16 # Number of rings
+cube_length = 10 # Length of cube in mm
+magnet_strength = 5492 #  Magnet strength in gauss
 
-ring_diameter = 92 #ring diameter in mm
-ring_length = 134 #ring length in mm
-plot_length= 40 #diameter length field map in mm
-dsv_diameter = 50 #diameter of the spherical volume in mm
-eclipse_ratio = 0.983 #minor axis/major axis ratio
+ring_diameter = 88 # Ring diameter in mm
+ring_length = 180 # Ring length in mm
+plot_length= 40 # Length of field map plotted in mm
+dsv = 50 # Diameter of the spherical volume in mm
+eclipse_ratio = 1 # Minor axis/major axis ratio
 
 # Configuration for second layer of rain
-ring_diameter_second_layer = 120
-n_magnet_second_layer = 20
+ring_diameter_second_layer = ring_diameter + 35
+n_magnet_second_layer = 6
 n_ring_second_layer = 2
-ring_length_second_layer = 30
+ring_length_second_layer = 90
 
-def field_compute(halbach):
-    angles = np.linspace(0, 360, n_magnets, endpoint=False)
-
+def generate_magnet(halbach):
     # Protect code if n_rings = 1
     if n_rings != 1:
-        ring_spacing = ring_length/(n_rings-1)
+        ring_spacing = ring_length / (n_rings - 1)
     else:
         ring_spacing = 1
 
-    z_starting_pos = -ring_length/2
+    # Define starting z position from the bottom, so center is at (0,0,0)
+    z_starting_pos = -ring_length / 2
+
+    # Loop to generate ring by ring
     for r in range(n_rings):
-        #setting XYZ position per ring
+        # Setting diameter for each ring for optimization
+        ring_diameter_array = [
+            ring_diameter ,
+            ring_diameter ,
+            ring_diameter ,
+            ring_diameter ,
+            ring_diameter + 5.3,
+            ring_diameter + 5.4,
+            ring_diameter + 4.9,
+            ring_diameter + 5.3,
+            ring_diameter + 5.3,
+            ring_diameter + 4.9,
+            ring_diameter + 5.4,
+            ring_diameter + 5.3,
+            ring_diameter ,
+            ring_diameter ,
+            ring_diameter ,
+            ring_diameter ,
+        ]
+
+        # Setting XYZ position per ring
         z_pos = z_starting_pos + r * ring_spacing
         y_pos = 0
-        x_pos = ring_diameter / 2
-        for ang in angles:
-            # Calculating Eclipse
-            a = ring_diameter / 2
-            b = a*eclipse_ratio
-            x_pos = (a*b)/(np.sqrt((b*np.cos(np.radians(ang)))**2 + (a*np.sin(np.radians(ang)))**2))
 
-            # Calculate position to set ring diameter
+        # Define the angles between each magnet in a circle
+        angles = np.linspace(0, 360, n_magnets, endpoint=False)
+
+        for ang in angles:
+            # Calculate Eclipse
+            a = ring_diameter_array[r] / 2
+            b = a * eclipse_ratio
+            x_pos = (a * b) / (np.sqrt((b * np.cos(np.radians(ang))) ** 2 + (a * np.sin(np.radians(ang))) ** 2))
+
+            if (r > 3) and (r < 12):
+                x_pos = ring_diameter_array[r] / 2
+
+            # Defining the position and rotation of magnet
             cube = magpy.magnet.Cuboid(
-                dimension=(cube_length,cube_length,cube_length),
-                magnetization=(magnet_strength/1.25663753*100,0,0), #converted using B = T/u
-                position=(x_pos,y_pos,z_pos)
+                dimension=(cube_length, cube_length, cube_length),
+                magnetization=(magnet_strength / 1.25663753 * 100, 0, 0),  # converted using B = T/u
+                position=(x_pos, y_pos, z_pos)
             )
             cube.rotate_from_angax(ang, 'z', anchor=0)
             cube.rotate_from_angax(ang, 'z')
             halbach.add(cube)
     return halbach
 
+
 def add_second_layer_ring(halbach):
     angles = np.linspace(0, 360, n_magnet_second_layer, endpoint=False)
     ring_spacing = ring_length_second_layer / (n_ring_second_layer - 1)
     z_starting_pos = -ring_length_second_layer / 2
     for r in range(n_ring_second_layer):
-        # setting XYZ position per ring
+        # Setting XYZ position per ring
         z_pos = z_starting_pos + r * ring_spacing
         x_pos = ring_diameter_second_layer / 2
         y_pos = 0
-        for a in angles:
+        for ang in angles:
+            # Calculating Eclipse
+            a = ring_diameter_second_layer / 2
+            b = a * eclipse_ratio
+            x_pos = (a * b) / (np.sqrt((b * np.cos(np.radians(ang))) ** 2 + (a * np.sin(np.radians(ang))) ** 2))
             # Calculate position to set ring diameter
             cube = magpy.magnet.Cuboid(
                 dimension=(cube_length, cube_length, cube_length),
                 magnetization=(magnet_strength / 1.25663753 * 100, 0, 0),  # converted using B = T/u
                 position=(x_pos, y_pos, z_pos)
             )
-            cube.rotate_from_angax(a, 'z', anchor=0)
-            cube.rotate_from_angax(a, 'z')
+            cube.rotate_from_angax(ang, 'z', anchor=0)
+            cube.rotate_from_angax(ang, 'z')
             halbach.add(cube)
     return halbach
 
 
-def calculate_ppm(halbach, dsv_diameter):
+def calculate_ppm(halbach, dsv):
     # Define the grid points over the DSV
-    r = dsv_diameter / 2
+    r = dsv / 2
     grid = np.mgrid[-r:r:20j, -r:r:20j, -r:r:20j].T.reshape(-1, 3)
+
+    # Spherical shape is defined
     distances = np.linalg.norm(grid, axis=1)
     grid = grid[distances <= r]
 
@@ -88,7 +119,7 @@ def calculate_ppm(halbach, dsv_diameter):
     B = halbach.getB(grid)
     B_magnitude = np.linalg.norm(B, axis=1)
 
-    # Calculate the average and maximum deviation
+    # Calculate the mean and maximum deviation
     B_avg = np.mean(B_magnitude)
     B_max_deviation = np.max(np.abs(B_magnitude - B_avg))
 
@@ -97,9 +128,8 @@ def calculate_ppm(halbach, dsv_diameter):
     return uniformity_ppm, B_avg, B_max_deviation
 
 def plot_halback(halbach):
-    # Change 3-D graph view
+    # Defining graph
     fig = halbach.show(backend='plotly', return_fig=True)
-
     # Update axis labels
     fig.update_layout(
         scene=dict(
@@ -108,23 +138,33 @@ def plot_halback(halbach):
             zaxis_title="Z-axis (mm)"
         )
     )
-
-    # Show the figure with updated axis labels
+    # Show the figure
     fig.show()
 
 # Compute and plot field on x-y grid
-def plot_xy(halbach, print_result=False, arrow=False,plot_show =True):
+def plot_result(halbach, print_result=False, arrow=False, plot_show=True):
     fig, axes = plt.subplots(1, 3, figsize=(18, 6))
-    z_positions = [-plot_length/2, 0, plot_length/2]
-    for z_pos, ax in zip(z_positions, axes):
-        grid = np.mgrid[-plot_length/2:plot_length/2:100j, -plot_length/2:plot_length/2:100j, z_pos:z_pos:1j].T[0]
-        X, Y, _ = np.moveaxis(grid, 2, 0)
+    # Set the z position view of the three graphs
+    z_positions = [-plot_length / 2, 0, plot_length / 2]
 
+    for z_pos, ax in zip(z_positions, axes):
+        # Grid is defined
+        grid = np.mgrid[-plot_length / 2:plot_length / 2:100j, -plot_length / 2:plot_length / 2:100j, z_pos:z_pos:1j].T[
+            0]
+
+        # Separate grid in to X and Y axis
+        X, Y, _ = np.moveaxis(grid, 2, 0)
         B = halbach.getB(grid)
-        Bx, By, _ = np.moveaxis(B, 2, 0)
-        Bamp = np.linalg.norm(B, axis=2)*1000
+
+        # Magnitude of field vector is taken and converted to mT
+        Bamp = np.linalg.norm(B, axis=2) * 1000
+
+        # Define field map
         pc = ax.contourf(X, Y, Bamp, levels=35, cmap="jet")
+
+        # Show field vector
         if arrow == True:
+            Bx, By, _ = np.moveaxis(B, 2, 0)
             ax.streamplot(X, Y, Bx, By, color="k", density=1.5, linewidth=1)
 
         # Add colorbar
@@ -138,45 +178,52 @@ def plot_xy(halbach, print_result=False, arrow=False,plot_show =True):
             aspect=1,
         )
 
-    # Calculate and display uniformity in ppm
-    uniformity_ppm, B_average, B_max_deviation = calculate_ppm(halbach, dsv_diameter)
-    plt.suptitle(f"Average field: {B_average*1000:.2f}mT   Max deviation: {B_max_deviation*1000:.2f}mT   Homogeneity: {uniformity_ppm:.0f} ppm over {dsv_diameter} mm DSV", fontsize=16)
+    # Calculate and display uniformity in ppm, field strength, and max deviation on graph
+    uniformity_ppm, B_average, B_max_deviation = calculate_ppm(halbach, dsv)
+    plt.suptitle(
+        f"Average field: {B_average * 1000:.2f}mT   Max deviation: {B_max_deviation * 1000:.2f}mT   Homogeneity: {uniformity_ppm:.0f} ppm over {dsv} mm DSV",
+        fontsize=16)
     plt.tight_layout()
 
-    # Printing for excel spread sheet comparison
+    # Print for excel spread sheet comparison. Have no effect on outcome.
     if print_result:
-        result = [f'{B_average*1000:.2f}', f'{uniformity_ppm:.0f}',f'{B_max_deviation*1000:.2f}', dsv_diameter, ring_diameter, ring_length, n_rings*n_magnets, n_magnets, n_rings, eclipse_ratio]
+        result = [f'{B_average * 1000:.2f}', f'{uniformity_ppm:.0f}', f'{B_max_deviation * 1000:.2f}', dsv,
+                  ring_diameter, ring_length, n_rings * n_magnets, n_magnets, n_rings, eclipse_ratio]
         formatted_data = '\t'.join(map(str, result))
-        print (formatted_data)
+        print(formatted_data)
+
     if plot_show == True:
         plt.show()
 
-# Add array to keep the data, and mark the optimized one. Lastly set halbach to optimized one.
-def optimize_eclipse(halbach, starting_eclipse_ratio,ending_eclipse_ratio,increments):
+# Set the eclipse ratio automatically per ring
+def optimize_eclipse(starting_eclipse_ratio,ending_eclipse_ratio,increments):
+    optimized_eclipse_array = []
     while starting_eclipse_ratio <= ending_eclipse_ratio:
         eclipse_ratio = starting_eclipse_ratio
-        temp_halbach = field_compute(halbach)
-        plot_xy(temp_halbach,print_result=True,arrow=True,plot_show=False)
+        temp_halbach = magpy.Collection()
+        temp_halbach = generate_magnet(temp_halbach)
+        homogeneity_result = plot_result(temp_halbach,print_result=True,arrow=False,plot_show=False)
+        optimized_eclipse_array.append([homogeneity_result, eclipse_ratio])
         starting_eclipse_ratio += increments
+    return optimized_eclipse_array
 
 # For Optimization
-'''
-for x in range(983, 984, 1):
+"""
+for x in range(995, 1001, 1):
     eclipse_ratio = x/1000
     halbach = magpy.Collection()
-    halbach = field_compute(halbach)
+    halbach = generate_magnet(halbach)
     plot_halback(halbach)
-    plot_xy(halbach, print_result=True, arrow=True, plot_show=True)
+    plot_result(halbach, print_result=True, arrow=True, plot_show=True)
+"""
 
-'''
 halbach = magpy.Collection()
 
-halbach = field_compute(halbach)
+#optimized_eclipse_array = optimize_eclipse(starting_eclipse_ratio = 0.98,ending_eclipse_ratio = 1,increments = 0.005)
 
-#optimize_eclipse(halbach, starting_eclipse_ratio = 0.95,ending_eclipse_ratio = 1,increments = 0.01)
-
+halbach = generate_magnet(halbach)
 #halbach = add_second_layer_ring(halbach)
 
 plot_halback(halbach)
 
-plot_xy(halbach, print_result=True, arrow=True)
+plot_result(halbach, print_result=True, arrow=True)
